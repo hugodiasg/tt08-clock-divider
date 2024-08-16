f86ca89d2a0caaa2f9a37faad6e8a23c54a9e0c6
Hugo Dias <hdg199@gmail.com>
2024-08-16 18:48:59 -0300

    create the test files

--- test/test.py
+++ test/test.py
@@ -24,17 +24,17 @@ async def test_project(dut):
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
+    # Set the input values, wait (200_000 * dut.ui_in.value) clock cycles, and check the output
+    dut._log.info("Test")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # While to check if the clock is being divided
+    CONST = 258000  # it should be equal to the CONST of src/clock_divider.v
+    cycles = 0
+    while True:
+        await ClockCycles(dut.clk, 1)
+        if dut.uo_out[0] == 1:
+            break
+        cycles += 1
+    print(f"took {cycles} cycles")
+    assert cycles == CONST / 2
+    
