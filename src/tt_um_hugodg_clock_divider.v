5ee452c31bf4c243f345220aabde75a50a20a3a6
Hugo Dias <hdg199@gmail.com>
2024-08-16 20:14:39 -0300

    updated src

--- src/tt_um_hugodg_clock_divider.v
+++ src/tt_um_hugodg_clock_divider.v
@@ -5,13 +5,13 @@
 
 `default_nettype none
 
-module tt_um_hugodg_clock_divider(
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+module tt_um_hugodg_clock_divider (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -19,7 +19,7 @@ module tt_um_hugodg_clock_divider(
   // All output pins must be assigned. If not used, assign to 0.
     assign uio_out[7:0] = 0; 
     assign uio_oe[7:0] = 8'b0;   
-    assign uo_out[7:1] = 0;  
+    assign uo_out[7:1] = 0; 
 
   // List all unused inputs to prevent warnings
     wire _unused = &{ena, 1'b0};
@@ -31,5 +31,4 @@ clock_divider clock_divider_inst(
     .scale(ui_in[7:0]),
     .clk_out(uo_out[0])
   );
-
 endmodule
